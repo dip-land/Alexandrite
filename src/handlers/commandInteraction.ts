9c113f7e4a2ee2497f0a9b42c1b72366fc7257e4
shhh7612 <yogscast.sebastian@gmail.com>
2022-04-09 22:38:32 -0500

    stuff

--- src/handlers/commandInteraction.ts
+++ src/handlers/commandInteraction.ts
@@ -2,6 +2,11 @@ import { BaseCommandInteraction, Client } from "discord.js";
 
 export default async (client: Client, interaction: BaseCommandInteraction): Promise<void> => {
     const command = client.commands.get(interaction.commandName);
+    const data = command.data;
+    const channel = interaction.channel;
+    if (data.disabled) return interaction.reply({ content: 'This command is currently disabled.', ephemeral: true });
+    if (data.permissions && !interaction.memberPermissions.has(data.permissions)) return interaction.reply({ content: 'You do not have the required permissions to use this command.', ephemeral: true });
+    if (data.nsfw && channel.type === 'GUILD_TEXT' && !channel.nsfw) return interaction.reply({ content: 'This command only works in NSFW channels.', ephemeral: true });
     try {
         command.default(client, interaction);
     } catch (error) {
---
+++ src/types/command.ts
@@ -0,0 +1,5 @@
+import { applicationCommand } from "./applicationCommand"
+export type command = {
+    data: applicationCommand,
+    default: Promise<void>
+}
