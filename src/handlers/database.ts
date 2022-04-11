b30f70767405d86d84a7d9355432f91cabcc3a31
shhh7612 <yogscast.sebastian@gmail.com>
2022-04-11 01:02:52 -0500

    Update database.ts

--- src/handlers/database.ts
+++ src/handlers/database.ts
@@ -1,41 +1,59 @@
 import { Message } from 'discord.js';
 import { Model, DataTypes } from 'sequelize';
 
-let database: Model = null;
+let users: Model = null;
+let guilds: Model = null;
 
-function add(userID: string) {
-    database.create({ userID: userID });
+function add(options: { userID?: string, guildID?: string }) {
+    if (options.userID) {
+        users.create({ userID: options.userID });
+    } else if (options.guildID) {
+        guilds.create({ guildID: options.guildID });
+    }
 }
 
-function delete_(userID: string) {
-    database.destroy({ where: { userID: userID } });
+function delete_(options: { userID?: string, guildID?: string }) {
+    if (options.userID) {
+        users.destroy({ where: { userID: options.userID } });
+    } else if (options.guildID) {
+        guilds.destroy({ where: { guildID: options.guildID } });
+    }
 }
 
 async function execute(message: Message) {
     if (message.author.bot) return;
-    let foundUser: User = await find(`U${message.author.id}; G${message.guildId};`);
+    let foundUser: User = await find({ userID: `U${message.author.id}; G${message.guildId};` });
     if (foundUser) {
         if (foundUser.xp >= ((foundUser.level * 1000) * 1.35)) {
-            update(foundUser.userID, 0, foundUser.level + 1, foundUser.messages + 1);
+            update({ user: { ID: foundUser.userID, xp: 0, level: foundUser.level + 1, messages: foundUser.messages + 1 } });
         } else {
-            update(foundUser.userID, foundUser.xp + Math.floor(Math.random() * 20) + 10, foundUser.level, foundUser.messages + 1);
+            update({ user: { ID: foundUser.userID, xp: foundUser.xp + Math.floor(Math.random() * 20) + 10, level: foundUser.level, messages: foundUser.messages + 1 } });
         }
     } else {
-        add(`U${message.author.id}; G${message.guildId};`);
+        add({ userID: `U${message.author.id}; G${message.guildId};` });
     }
 }
 
-async function find(userID: string) {
-    const foundUser = await database.findOne({ where: { userID: userID } });
-    try {
-        return foundUser.dataValues;
-    } catch (error) {
-        return foundUser;
+async function find(options: { userID?: string, guildID?: string }) {
+    if (options.userID) {
+        const foundUser = await users.findOne({ where: { userID: options.userID } });
+        try {
+            return foundUser.dataValues;
+        } catch (error) {
+            return foundUser;
+        }
+    } else if (options.guildID) {
+        const foundUser = await guilds.findOne({ where: { guildID: options.guildID } });
+        try {
+            return foundUser.dataValues;
+        } catch (error) {
+            return foundUser;
+        }
     }
 }
 
 function start(db) {
-    database = db.define('Users', {
+    users = db.define('Users', {
         userID: {
             type: DataTypes.STRING,
             unique: true,
@@ -55,26 +73,45 @@ function start(db) {
             defaultValue: 0,
             allowNull: false,
         }
+    });
+    guilds = db.define('Guilds', {
+        guildID: {
+            type: DataTypes.STRING,
+            unique: true,
+        }
     })
 }
 
 function sync() {
-    database.sync();
+    users.sync();
+    guilds.sync();
 }
 
-function update(userID: string, xp: number, level: number, messages: number) {
-    database.update(
-        {
-            xp: xp,
-            level: level,
-            messages: messages,
-        },
-        {
-            where: {
-                userID: userID,
+function update(options: { user?: { ID: string, xp: number, level: number, messages: number }, guild?: { ID: string } }) {
+    if (options.user) {
+        users.update(
+            {
+                xp: options.user.xp,
+                level: options.user.level,
+                messages: options.user.messages,
+            },
+            {
+                where: {
+                    userID: options.user.ID,
+                }
             }
-        }
-    );
+        );
+    } else if (options.guild) {
+        guilds.update(
+            {
+            },
+            {
+                where: {
+                    guildID: options.guild.ID,
+                }
+            }
+        );
+    }
 }
 
 declare module "sequelize" {
